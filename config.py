04a366c09cfc5e0dd2dbf4026fc83680b6bf15ab
POFIG1ST <64265470+POFIG1ST@users.noreply.github.com>
2025-05-08 03:23:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,21 @@
+#Данные с сайта https://my.telegram.org/apps
+api_id =
+api_hash = ''
+
+ # Тут пишешь text/sticker в зависимости от того, че хочешь отправлять
+content_type = 'text'
+
+# Тут текст пишешь если спамишь текстом
+sent_text = 'Алкоголик лучший'
+
+# Тут указываешь stickerid получить можно тут @LeadConverterToolkitBot
+sticker_id = 'CAACAgIAAxkBAAEDOfxlu_4LgqLrLULjv8rEV4EW79ByRwACcQADto9KCaZq9nltOJJfNAQ'
+
+# Стандатрная задержка 2 секунды чтобы не уходить в спам самим ботом
+before_next_sleep = 2
+
+# время задержки после отправки /next , нужно если бот отвечает Новые пользователи не м>
+after_next_sleep = 2
+
+
 
